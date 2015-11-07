956bc579e30cd0884458c0004ca2d699fcdb3f79
Anton Titkov <atlascoder@gmail.com>
2015-11-07 23:43:41 +0000

    fixing to pass test/unit/issue_test.rb

--- lib/tracker_hider_issue_patch.rb
+++ lib/tracker_hider_issue_patch.rb
@@ -15,19 +15,20 @@ module TrackerHiderIssuePatch
   module ClassMethods
     
     def visible_condition_with_tracker_hider (user, *args)
+      user_id = user.id || 0
       visible_condition_without_tracker_hider(user, *args) +
         " AND NOT EXISTS( "+
           "SELECT * from hidden_trackers WHERE (" + 
               "((hidden_trackers.project_id IN (SELECT project_id FROM enabled_modules WHERE enabled_modules.name='tracker_hider'))" +
               " AND issues.project_id=project_id" +
               " AND issues.tracker_id=tracker_id" +
-              " AND user_id=#{user.id})" +
+              " AND user_id=#{user_id})" +
             " OR (" +
               "(hidden_trackers.project_id IN (SELECT project_id FROM enabled_modules WHERE enabled_modules.name='tracker_hider'))" +
               " AND issues.project_id=project_id" +
               " AND issues.tracker_id=tracker_id" +
               " AND (hidden_trackers.role_id IN (SELECT role_id FROM member_roles WHERE member_id IN ("+
-                "SELECT id from members WHERE user_id=#{user.id} AND project_id=hidden_trackers.project_id" +
+                "SELECT id from members WHERE user_id=#{user_id} AND project_id=hidden_trackers.project_id" +
               ")))" +
             ")" +
           ")" +
