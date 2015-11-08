16611d7e06d01941f59d933dde20469db3998dc8
Anton Titkov <atlascoder@gmail.com>
2015-11-08 17:28:45 +0000

    Erronous SQL Syntax fixed

--- lib/tracker_hider_issue_patch.rb
+++ lib/tracker_hider_issue_patch.rb
@@ -23,11 +23,11 @@ module TrackerHiderIssuePatch
       ## Match for selected user_id
                   " hts.user_id=#{user_id} " +
       ## Match for selected role_id (BUT not for Anonymous and Not Member)
-                " OR ((hts.role_id NOT NULL) AND hts.role_id IN (SELECT mr.role_id FROM member_roles AS mr INNER JOIN members AS m ON mr.member_id=m.id " + 
+                " OR ((hts.role_id IS NOT NULL) AND hts.role_id IN (SELECT mr.role_id FROM member_roles AS mr INNER JOIN members AS m ON mr.member_id=m.id " + 
                   " WHERE m.user_id=#{user_id} AND m.project_id=issues.project_id)) " +
       ## Match for Anonymous user
                 " OR (hts.role_id=2 AND 2=#{user_id})" +
-      ## Match for Not Member user
+      ## Match for Not Member user (Anonymous isn't a member as well, yeah)
                 " OR ((hts.role_id=1) AND NOT EXISTS(SELECT mr.role_id FROM member_roles AS mr INNER JOIN members AS m ON mr.member_id=m.id " + 
                   " WHERE m.user_id=#{user_id} AND m.project_id=issues.project_id)) " +
               ")" +
