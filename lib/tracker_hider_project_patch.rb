f7ba84d871179af973e78d2528ad36a541ba06ca
Anton Titkov <atlascoder@gmail.com>
2015-11-18 02:58:13 +0000

    Crash for not memeber fixed

--- lib/tracker_hider_project_patch.rb
+++ lib/tracker_hider_project_patch.rb
@@ -5,12 +5,23 @@ module TrackerHiderProjectPatch
   module IM
     
     def trackers_with_filter
-      trackers_without_filter.where("NOT EXISTS (SELECT * FROM hidden_trackers AS hts WHERE trackers.id=hts.tracker_id AND hts.project_id IS NULL AND hts.user_id IS NULL AND hts.role_id IS NOT NULL AND hts.role_id IN (#{my_roles_in_project.join(',')}))")
+      mrp = my_roles_in_project
+      if mrp.empty? then
+        trackers_without_filter.where("NOT EXISTS (SELECT * FROM hidden_trackers AS hts WHERE trackers.id=hts.tracker_id AND hts.project_id IS NULL AND hts.user_id IS NULL AND hts.role_id IS NOT NULL AND hts.role_id IN (1, 2))")
+      else
+        trackers_without_filter.where("NOT EXISTS (SELECT * FROM hidden_trackers AS hts WHERE trackers.id=hts.tracker_id AND hts.project_id IS NULL AND hts.user_id IS NULL AND hts.role_id IS NOT NULL AND hts.role_id IN (#{mrp.join(',')}))")
+      end
     end
     
     def rolled_up_trackers_with_filter
       t = []
-      hidden_a = HiddenTracker.where("project_id IS NULL AND user_id IS NULL AND role_id IS NOT NULL AND role_id IN (#{my_roles_in_project.join(',')})").uniq.collect{|t| t.tracker_id}
+      hidden_a = []
+      mrp = my_roles_in_project
+      if mrp.empty? then
+        hidden_a = HiddenTracker.where("project_id IS NULL AND user_id IS NULL AND role_id IS NOT NULL AND role_id IN (1, 2)").uniq.collect{|t| t.tracker_id}
+      else  
+        hidden_a = HiddenTracker.where("project_id IS NULL AND user_id IS NULL AND role_id IS NOT NULL AND role_id IN (#{mrp.join(',')})").uniq.collect{|t| t.tracker_id}
+      end
       rolled_up_trackers_without_filter.each {|tr| t.push(tr) unless hidden_a.include?(tr[:id])}
       t
     end
