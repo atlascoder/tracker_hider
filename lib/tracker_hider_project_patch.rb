2c831bbaa49444ecc3fb5d322e1fa9d34c0b9847
Anton Titkov <atlascoder@gmail.com>
2015-11-18 03:10:57 +0000

    Added distinction Anonymous and Non Memeber

--- lib/tracker_hider_project_patch.rb
+++ lib/tracker_hider_project_patch.rb
@@ -6,28 +6,26 @@ module TrackerHiderProjectPatch
     
     def trackers_with_filter
       mrp = my_roles_in_project
-      if mrp.empty? then
-        trackers_without_filter.where("NOT EXISTS (SELECT * FROM hidden_trackers AS hts WHERE trackers.id=hts.tracker_id AND hts.project_id IS NULL AND hts.user_id IS NULL AND hts.role_id IS NOT NULL AND hts.role_id IN (1, 2))")
-      else
-        trackers_without_filter.where("NOT EXISTS (SELECT * FROM hidden_trackers AS hts WHERE trackers.id=hts.tracker_id AND hts.project_id IS NULL AND hts.user_id IS NULL AND hts.role_id IS NOT NULL AND hts.role_id IN (#{mrp.join(',')}))")
-      end
+      trackers_without_filter.where("NOT EXISTS (SELECT * FROM hidden_trackers AS hts WHERE trackers.id=hts.tracker_id AND hts.project_id IS NULL AND hts.user_id IS NULL AND hts.role_id IS NOT NULL AND hts.role_id IN (#{mrp.join(',')}))")
     end
     
     def rolled_up_trackers_with_filter
       t = []
       hidden_a = []
       mrp = my_roles_in_project
-      if mrp.empty? then
-        hidden_a = HiddenTracker.where("project_id IS NULL AND user_id IS NULL AND role_id IS NOT NULL AND role_id IN (1, 2)").uniq.collect{|t| t.tracker_id}
-      else  
-        hidden_a = HiddenTracker.where("project_id IS NULL AND user_id IS NULL AND role_id IS NOT NULL AND role_id IN (#{mrp.join(',')})").uniq.collect{|t| t.tracker_id}
-      end
+      hidden_a = HiddenTracker.where("project_id IS NULL AND user_id IS NULL AND role_id IS NOT NULL AND role_id IN (#{mrp.join(',')})").uniq.collect{|t| t.tracker_id}
       rolled_up_trackers_without_filter.each {|tr| t.push(tr) unless hidden_a.include?(tr[:id])}
       t
     end
     
     def my_roles_in_project
-      members.where(user: User.current).collect{|r| r.id}
+      m_s = []
+      if User.current.anonymous? then
+        m_s.push
+      else
+        m_s = members.where(user: User.current).collect{|r| r.id}
+      end
+      m_s.empty? ? [1] : m_s 
     end
     
   end
