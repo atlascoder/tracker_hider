62ff14a1be4585aad44a8ee2057ec5670f8b52ce
atlascoder <atlascoder@gmail.com>
2015-12-26 01:53:13 +0200

    fixed access to hidden issue by URL

--- lib/tracker_hider_issue_patch.rb
+++ lib/tracker_hider_issue_patch.rb
@@ -4,11 +4,25 @@ module TrackerHiderIssuePatch
   def self.included(base)
     
     base.extend(ClassMethods)
+    base.send(:include, InstanceMethods)
     
     base.class_eval do
+
+      alias_method :visible_without_th, :visible?
+      alias_method :visible?, :visible_with_th
+
       class << self
         alias_method_chain :visible_condition, :tracker_hider
       end
+    end
+  end
+
+  module InstanceMethods
+    def visible_with_th(u=nil)
+      usr = u || User.current
+      users_roles = usr.members.where(project_id: project.id).collect{|m| MemberRole.find(m.id).role_id}
+      hf = HiddenTracker.where("tracker_id='#{tracker.id}' AND project_id IS NULL AND user_id IS NULL AND(role_id IN (#{users_roles.join(',')}))").present?
+      return (hf ? false : visible_without_th(usr))
     end
   end
   
