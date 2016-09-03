1e49602ef8076239b21851e8b2354d87c59f6643
AtlasCoder Studio <atlascoder@gmail.com>
2016-09-03 22:07:07 +0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :tracker_hider do
   name 'Tracker Hider plugin for Redmine'
   author 'Anton Titkov'
   description 'The plugin allows to hide specified tracker for cpecified users within a project'
-  version '0.0.1'
+  version '0.1.0'
   url 'https://github.com/atlascoder/tracker_hider'
   author_url 'https://github.com/atlascoder'
   
